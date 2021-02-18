4ac5a0d1939ac9bbe87b7cfc690d1c3a0993763f
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2021-02-18 18:40:45 +0100

    add middleware jwt security

--- server/middlewares.go
+++ server/middlewares.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 //logRequestMiddleware give the path of the request in console
@@ -24,3 +29,32 @@ func logRequestMiddleware(next http.HandlerFunc) http.HandlerFunc {
 // 		j.HandlerWithNext(w, r, next)
 // 	}
 // }
+
+func (s *Server) middleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(authHeader) != 2 {
+			fmt.Println("Malformed token")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Malformed Token"))
+		} else {
+			jwtToken := authHeader[1]
+			token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte(JWT_APP_KEY), nil
+			})
+			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				ctx := context.WithValue(r.Context(), "props", claims)
+				// Access context values in handlers like this
+				// props, _ := r.Context().Value("props").(jwt.MapClaims)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			} else {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized"))
+			}
+		}
+	})
+}
--- server/routes.go
+++ server/routes.go
@@ -5,7 +5,7 @@ func (s *Server) Routes() {
 	s.Router.HandleFunc("/", s.handleIndex()).Methods("GET")
 	s.Router.HandleFunc("/api/user/login", s.handleUserLogin()).Methods("POST")
 	s.Router.HandleFunc("/api/user/create", s.handleUserCreate()).Methods("POST")
-	s.Router.HandleFunc("/api/movies", s.handleMovieList()).Methods("GET")
+	s.Router.HandleFunc("/api/movies", s.middleware(s.handleMovieList())).Methods("GET")
 	s.Router.HandleFunc("/api/movies", s.handleMovieCreate()).Methods("POST")
 	s.Router.HandleFunc("/api/movies/{id:[0-9]+}", s.handleMovieDetail()).Methods("GET")
 }
